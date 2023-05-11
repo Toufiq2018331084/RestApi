b5b04612d99347304e52014ca2f13991bc34cbf6
Toufiq2018331084 <52928065+Toufiq2018331084@users.noreply.github.com>
2023-05-11 19:06:53 +0600

    complete

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {v4 as uuidv4} from 'uuid';
+import {createUser,getUser,deleteUser,updateUser} from '../controllers/users.js';
 
 const router = express.Router();
 
@@ -9,32 +9,13 @@ router.get('/', (req,res)=> {
     res.send(users);
 });
 
-router.post('/',(req,res)=>{
-    users.push({...req.body, id: uuidv4()});
-    res.send("added hopefully");
-});
+router.post('/',createUser);
 
-router.get('/:id',(req,res)=>{
-    const {id} =req.params;
-    const foundUser = users.find((user)=> user.id ===id);
-    res.send(foundUser);    
-});
+router.get('/:id',getUser);
 
-router.delete('/:id',(req,res)=>{
-    const {id} = req.params;
-    users = users.filter((user)=>user.id!=id);
-    res.send(users);
-});
+router.delete('/:id',deleteUser);
 
-router.patch('/:id',(req,res)=>{
-    const {id} = req.params;
-    const user = users.find((user)=>user.id===id);
-    const {firstName, lastName, age} = req.body;
-    if(firstName)user.firstName=firstName;
-    if(lastName)user.lastName=lastName;
-    if(age)user.age=age;
-    res.send(users);
-});
+router.patch('/:id',updateUser);
 
 
 export default router;
